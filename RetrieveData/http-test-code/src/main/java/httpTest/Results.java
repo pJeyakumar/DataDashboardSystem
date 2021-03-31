4ff34920f3389e335ddc0113e7b21132860a267d
John Palmer <32370730+jpalmer37@users.noreply.github.com>
2021-03-31 19:20:21 -0400

    added new exception; updated results

--- RetrieveData/http-test-code/src/main/java/httpTest/Results.java
+++ RetrieveData/http-test-code/src/main/java/httpTest/Results.java
@@ -2,9 +2,20 @@ package httpTest;
 
 public class Results {
 	
-	public Results() {
+	private Data[] storedData; 
+	private int numOfSeries; 
+	public Results(Data[] inputData) {
 		
+		storedData = inputData;
+		numOfSeries = inputData.length;
 	}
 	
+	public Data getData(int i) throws OutOfBoundsException{
+		if (i > numOfSeries) {
+			 throw new OutOfBoundsException("Data entry not found");
+		}
+		
+		return storedData[i];
+	}
 	
 }

f578de7a2f8b87b7fac26fdca4816c9bb0c5f1e0
John Palmer <32370730+jpalmer37@users.noreply.github.com>
2021-03-31 18:09:02 -0400

    updated data and reader

--- RetrieveData/http-test-code/src/main/java/httpTest/Data.java
+++ RetrieveData/http-test-code/src/main/java/httpTest/Data.java
@@ -7,27 +7,31 @@ import java.util.ArrayList;
  * DATE: 2021-03-30
  * DESCRIPTION: Class that will be used to store the arrays of data from the JSON file that was fetched from the world bank
  */
-public class Data<A,B>
+public class Data
 {	
 	// Private instance variables
 	private String typeA;
-	private String typeB;
-	private ArrayList<A> firstSet;
-	private ArrayList<B> secondSet;
+	private ArrayList<Double> firstSet;
+	private ArrayList<Integer> secondSet;
 	
 	// Constructor class with dataType, dataArray1 and dataArray2 as parameters
-	public Data(ArrayList<A> data1, ArrayList<B> data2)
+	public Data(ArrayList<Double> data1)
+	{
+		this.firstSet = data1;
+	}
+	
+	public Data(ArrayList<Double> data1, ArrayList<Integer> data2)
 	{
 		this.firstSet = data1;
 		this.secondSet = data2;
 	}
 	// Setter method for firstSet var
-	public void  setFirst(ArrayList<A> data) 
+	public void  setFirst(ArrayList<Double> data) 
 	{
 		this.firstSet = data;
 	}
 	// Setter method for secondSet var
-	public void setSecond(ArrayList<B> data)
+	public void setSecond(ArrayList<Integer> data)
 	{
 		this.secondSet = data;
 	}
@@ -35,18 +39,14 @@ public class Data<A,B>
 	public void setTypeA(String type) 
 	{
 		this.typeA = type;
-	}	// Setter method for typeB var
-	public void setTypeB(String type) 
-	{
-		this.typeB = type;
 	}
 	// Getter method for firstSet
-	public ArrayList<A> getFirst() 
+	public ArrayList<Double> getFirst() 
 	{
 		return this.firstSet;
 	}
 	// Getter method for secondSet
-	public ArrayList<B> getSecond() 
+	public ArrayList<Integer> getSecond() 
 	{
 		return this.secondSet;
 	}
@@ -54,10 +54,6 @@ public class Data<A,B>
 	public String getTypeA() 
 	{
 		return this.typeA;
-	}	// Getter method for tyoe
-	public String getTypeB() 
-	{
-		return this.typeB;
-	}
+	}	// Getter method for type
 }
 

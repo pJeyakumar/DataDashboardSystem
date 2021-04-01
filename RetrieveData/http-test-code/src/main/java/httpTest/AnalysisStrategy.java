4ac4ae2e7bd2e2315df36b3c7f2f9308066a962c
Shoumik Shill <sshill@uwo.ca>
2021-04-01 18:37:53 -0400

    Updated AnalysisStrategy skeleton

--- RetrieveData/http-test-code/src/main/java/httpTest/AnalysisStrategy.java
+++ RetrieveData/http-test-code/src/main/java/httpTest/AnalysisStrategy.java
@@ -1,6 +1,10 @@
+package httpTest;
+
+import java.io.*;
+import java.util.ArrayList;
 
 /**
- * @author Group 46
+ * @author Group 46 - Allan Zhang, John Palmer, Piranavan Jeyakumar, Shoumik Shill
  * 
  * Analysis Strategy Class
  * Functional Class that:
@@ -9,16 +13,21 @@
  *   3. (Output) Populates this Processed Data to a Results Object
  * Called by
  *  - Computational Server class
- * 
+ * Data:
+ * - Reader - the reader to get original data from
+ * - Results - the
  */
 
 public class AnalysisStrategy {
 	
-	// Reader class which will return Data objects
+	// Reader class which will return unprocessed Data objects
 	private Reader reader;
+	// Results class which will be populated with processed Data
+	private Results results;
 	
-	private Data[] dataArray;
-	private Data[] processedDataArray;
+	// Arrays to hold the Data to be processed and the Data that is processed
+	private ArrayList<Data> dataArray;
+	private ArrayList<Data> processedDataArray;
 	
 	// Constructor - to be created by Computational Server class
 	public AnalysisStrategy() {
@@ -36,48 +45,37 @@ public class AnalysisStrategy {
 	*/
 	public void doAnalysis(Selection selection) {
 		
+		// Get the Data from Reader, store in tempData
+		Data tempData = retrieveData(selection);
+		
+		// PLACEHOLDER
+		ArrayList<Double> pData1 = new ArrayList<Double>();
+		this.processedDataArray[0] = new Data(pData1);
 		
 		
-	}
+		populateResults(this.processedDataArray[0]);
+		
+	} // End Do Analysis Method
 	
 	/**
-	 * Retrieve Data
+	 * Retrieve Data Method
 	 * To be called from Do Analysis to get the data to be processed
-	 * @return Data
+	 * @return Data - a single Data object
 	 */
-	public Data retrieveData() {
+	public Data retrieveData(Selection selection) {
 		
-		
-		// return this.reader.getData();
-	}
+		return this.reader.retrieveData(selection);
+	} // End Retrieve Data method
 	
 	/**
-	 * Populate Results
+	 * Populate Results Method
 	 * @param Data to be Processed
 	 */
-	public populateResults() {
+	public Results populateResults(ArrayList<Data> processedData) {
 		
-		this.processedData
-		
-	}
+		Results returnResults = null;
 	
-	/**
-	 * package httpTest;
-
-	abstract class Analysis {
-	
-	public Data[] dataArray; 
-	public int numOfSeries; 
-
-	// Main Method 
-	public abstract void doAnalysis(Selection userChoice);
-	
-	public abstract Data retrieveData(Selection userChoice);
-	
-	public Results populateResults(Data[] inputData) {
-		
-	}
-}	
-	 */
+		return returnResults;
+	} // End Populate Results method
 
 } // END CLASS
